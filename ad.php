46fe1eb654d7deb8de6921e77cb3963094c97d84
asvec6767 <90004068+asvec6767@users.noreply.github.com>
2023-04-30 08:59:54 +0300

    Add files via upload

---
+++ ad.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>О нас</title>
+    <link rel="stylesheet" href="style_work.css">
+    <link rel="stylesheet" href="tape_style.css">
+</head>
+<body>
+    <?php include "header.php";?>
+    <br><br><br><br>
+
+    <h1 style="text-align: center;"> Ваша реклама </h1>
+    <div class="card">
+        <img src="ad.jpg" alt="">
+        <div class= "card-content" >
+            <h3 class="card-name">Рекламное предложение</h3>
+            <h6 class="card-text">Вы можете расположить вашу компанию, в поиске, тем самым, привлекая новых клиентов. Для подробной информации звоните нам прямо сейчас: 8-684-83</h6>
+        </div>
+    </div>
+</body>
+</html>
